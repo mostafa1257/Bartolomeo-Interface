a2130ae18ae9b62ec9a8ec6c8a8d183c48227d4a
mostafa <mostafaaheshamm@gmail.com>
2022-06-28 20:28:43 +0200

    added driverlib files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Bartolomeo-Interface
 Repository for Bartolomeo Interface Software Development
+
+$ mkdir build
+$ cd build
+$ cmake ..
+$ make main #compile example
+$ make upload_main #upload to launchpad
--- main.c
+++ main.c
@@ -1,10 +1,19 @@
-// example program for launchpad, blink two leds, how original!
-
-#include <msp430.h>
-#include "FreeRTOS.h"
-#include "task.h"
+#include "msp430.h"
 
 int main()
 {
-	WDTCTL = WDTPW | WDTHOLD;
+	WDTCTL = WDTPW | WDTHOLD; // disable watchdog
+	
+	// GPIO_setAsOutputPin(GPIO_PORT_P10, GPIO_PIN6 + GPIO_PIN7);
+	P10DIR = (BIT7 | BIT6);
+	P10OUT &= ~BIT7;
+	P10OUT &= ~BIT6; // start with P1.0 on
+	// P10OUT &= ~BIT6; // and P1.6 off
+	
+	// while(1) //switch between P1.0 and P1.6 every seconds, until heat death of the universe
+	// {
+		P10OUT = BIT6;
+	// }
+
+	return 0;
 }
